e7718fde55bc42baa842be06c8873a3f5db18578
Tom Hicks <hickst@tohono.com>
2020-05-23 21:28:35 -0700

    Use sink arg to write headers to JSON output. Add stubs for pickle output. Generate/use output filepath.

--- imdtk/tools/headers.py
+++ imdtk/tools/headers.py
@@ -1,15 +1,19 @@
 #
 # Class for extracting header information from FITS files.
 #   Written by: Tom Hicks. 5/23/2020.
-#   Last Modified: Initial creation.
+#   Last Modified: Use sink arg to write headers to JSON output. Add stubs for pickle output.
+#                  Generate/use output filepath.
 #
 import os
 import sys
+import datetime
+import json
 import logging as log
 
 from astropy.io import fits
 
 from config.settings import OUTPUT_DIR
+import imdtk.core.file_utils as file_utils
 import imdtk.core.fits_utils as fits_utils
 # from imdtk.core.i_tool import ITool         # LATER: IMPLEMENT and uncomment
 
@@ -32,11 +36,18 @@ class HeadersTool ():
         # Debug setting: when true, show internal information for debugging.
         self._DEBUG = args.get('debug', False)
 
+        # Path to a readable FITS image file from which to extract metadata.
+        self._fits_file = args.get('fits_file')
+
         # An output file created within the output directory.
         self._output_file = None
 
         # Output format for the information when output.
         self._output_format = args.get('output_format') or 'json'
+
+        # Where to send the processing results from this tool.
+        self._output_sink = args.get('output_sink')
+
 
 
     #
@@ -46,7 +57,7 @@ class HeadersTool ():
     def cleanup (self):
         """ Do any cleanup/shutdown tasks necessary for this instance. """
         if (self._DEBUG):
-            print("({}).cleanup)".format(self.TOOL_NAME))
+            print("({}.cleanup)".format(self.TOOL_NAME))
         if (self._output_file is not None):
             self._output_file.close()
             self._output_file = None
@@ -60,15 +71,16 @@ class HeadersTool ():
 
 
     def process (self):
-        """ Perform the main work of the tool and return the results as a Python structure. """
-
+        """
+        Perform the main work of the tool and return the results as a Python structure.
+        """
         if (self._DEBUG):
             print("({}.process): ARGS={}".format(self.TOOL_NAME, self.args))
 
         # process the given, validated FITS file
         fits_file = self.args.get('fits_file')
         if (self._VERBOSE):
-            print("({}.process): Processing FITS file '{}'".format(self.TOOL_NAME, fits_file))
+            print("({}): Processing FITS file '{}'".format(self.TOOL_NAME, fits_file))
 
         ignore_list = self.args.get('ignore_list', [])
         which_hdu = self.args.get('which_hdu', 0)
@@ -80,12 +92,12 @@ class HeadersTool ():
                 else:
                     hdrs = fits_utils.get_header_fields(hdus_list, which_hdu)
 
-                if (hdrs is None):
-                    errMsg = "({}.process): Unable to read metadata from FITS file '{}'.".format(self.TOOL_NAME, fits_file)
-                    log.error(errMsg)
-                    raise RuntimeError(errMsg)
+            if (hdrs is None):
+                errMsg = "({}.process): Unable to read metadata from FITS file '{}'.".format(self.TOOL_NAME, fits_file)
+                log.error(errMsg)
+                raise RuntimeError(errMsg)
 
-                return hdrs                 # return the results of processing
+            return hdrs                         # return the results of processing
 
         except Exception as ex:
             errMsg = "({}.process): Exception while reading metadata from FITS file '{}': {}.".format(self.TOOL_NAME, fits_file, ex)
@@ -96,19 +108,57 @@ class HeadersTool ():
     def output_results (self, results):
         """ Output the given results in the selected format. """
 
-        # TODO: IMPLEMENT WRITING OUTPUT AS JSON
-        if (self._VERBOSE):
-            print("({}): HEADERS: {}".format(self.TOOL_NAME, results)) # REMOVE LATER
+        sink = self._output_sink
+        if (sink == 'file'):                # if output file specified
+            self._output_file = open(self.gen_output_file_path(), 'w')
+        else:                               # else default to standard output
+            self._output_file = sys.stdout
 
-        # TODO: maybe use LATER?
-        # if (self._output_format != 'db'):   # if writing to a file
-        #     out_file_path = self.gen_output_file_path(OUTPUT_DIR)
-        #     self._output_file = open(out_file_path, 'w')
+        out_fmt = self._output_format
+        if (out_fmt == 'json'):
+            self.output_JSON(results)
+        elif (out_fmt == 'pickle'):
+            self.output_pickle(results)
+        else:
+            errMsg = "({}.process): Invalid output format '{}'.".format(self.TOOL_NAME, out_fmt)
+            log.error(errMsg)
+            raise ValueError(errMsg)
+
+        if (self._VERBOSE):
+            out_dest = sink                 # default to current sink value
+            if (sink == 'file'):            # reset value if necessary
+                out_dest = self._output_file.name
+            print("({}): Results output to '{}'".format(self.TOOL_NAME, out_dest))
+
 
 
     #
     # Non-interface Methods
     #
+
+    def gen_output_file_path (self, out_dir=OUTPUT_DIR):
+        """
+        Return a unique output filepath, within the specified output directory,
+        for the result file. Use the given file_name string and extension to create the name.
+        """
+        time_now = datetime.datetime.now()
+        now_str = time_now.strftime("%Y%m%d_%H%M%S-%f")
+        fname = file_utils.filename_core(self._fits_file)
+        extension = self._output_format
+        return "{0}/{1}_{2}.{3}".format(out_dir, fname, now_str, extension)
+
+
+    def output_JSON (self, results):
+        # TODO: merge results into a larger structure, including fits_file info
+
+        json.dump(results, self._output_file, indent=2)
+        self._output_file.write('\n')
+
+
+    def output_pickle (self, results):
+        # TODO: IMPLEMENT WRITING OUTPUT AS Pickle
+        pass
+
 
     def to_JSON (self, fields_info):
         """ Return the given field information formatted as a JSON string. """
