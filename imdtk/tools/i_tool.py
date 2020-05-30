bd6af625c8a2b67d750bf2abccec5a90fabd2c27
Tom Hicks <hickst@tohono.com>
2020-05-30 15:39:17 -0700

    Update to work directory. Add tool name arg to output filename. Add output extents constant.

--- imdtk/tools/i_tool.py
+++ imdtk/tools/i_tool.py
@@ -1,13 +1,16 @@
 #
 # Abstract class defining the interface for tool components.
 #   Written by: Tom Hicks. 5/27/2020.
-#   Last Modified: Refactor file path generator here.
+#   Last Modified: Update to work directory. Add tool name arg to output filename. Add output extents constant.
 #
 import abc
 import datetime
 
-from config.settings import OUTPUT_DIR
+from config.settings import WORK_DIR
 import imdtk.core.file_utils as file_utils
+
+
+OUTPUT_EXTENTS = [ '.json', '.pickle' ]
 
 
 class IImdTool (abc.ABC):
@@ -37,7 +40,7 @@ class IImdTool (abc.ABC):
 
 
 
-    def gen_output_file_path (self, file_path, extension, out_dir=OUTPUT_DIR):
+    def gen_output_file_path (self, file_path, extension, tool_name='', out_dir=WORK_DIR):
         """
         Return a unique output filepath, within the specified output directory,
         for the result file. Use the given file_path string and extension to create the name.
@@ -45,4 +48,5 @@ class IImdTool (abc.ABC):
         time_now = datetime.datetime.now()
         now_str = time_now.strftime("%Y%m%d_%H%M%S-%f")
         fname = file_utils.filename_core(file_path)
-        return "{0}/{1}_{2}.{3}".format(out_dir, fname, now_str, extension)
+        tname = '_'+tool_name if tool_name else ''
+        return "{0}/{1}{2}_{3}.{4}".format(out_dir, fname, tname, now_str, extension)
