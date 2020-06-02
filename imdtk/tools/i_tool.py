bf4d5374e88019734088d34230667df79a1001e8
Tom Hicks <hickst@tohono.com>
2020-06-01 21:41:16 -0700

    Move methods to output JSON and pickle here. Add name strings for standard file descriptors.

--- imdtk/tools/i_tool.py
+++ imdtk/tools/i_tool.py
@@ -1,16 +1,23 @@
 #
 # Abstract class defining the interface for tool components.
 #   Written by: Tom Hicks. 5/27/2020.
-#   Last Modified: Update to work directory. Add tool name arg to output filename. Add output extents constant.
+#   Last Modified: Move methods to output JSON and pickle here. Add name strings for standard file descriptors.
 #
 import abc
 import datetime
+import json
+import pickle
+import sys
 
 from config.settings import WORK_DIR
 import imdtk.core.file_utils as file_utils
 
 
 OUTPUT_EXTENTS = [ '.json', '.pickle' ]
+
+STDIN_NAME = 'standard input'
+STDERR_NAME = 'standard error'
+STDOUT_NAME = 'standard output'
 
 
 class IImdTool (abc.ABC):
@@ -50,3 +57,25 @@ class IImdTool (abc.ABC):
         fname = file_utils.filename_core(file_path)
         tname = '_'+tool_name if tool_name else ''
         return "{0}/{1}{2}_{3}.{4}".format(out_dir, fname, tname, now_str, extension)
+
+
+    def output_JSON (self, data, file_path=None):
+        """ Jsonify and write the given data structure to the given file path or standard output. """
+        if (file_path is not None):         # if output file specified
+            outfile = open(file_path, 'w')
+            json.dump(data, outfile, indent=2)
+            outfile.write('\n')
+            outfile.close()
+        else:                               # else write to standard output
+            json.dump(data, sys.stdout, indent=2)
+            sys.stdout.write('\n')
+
+
+    def output_pickle (self, data, file_path=None):
+        """ Pickle and write the given data structure to the given file path or standard output. """
+        if (file_path is not None):         # if output file specified
+            outfile = open(file_path, 'wb')
+            pickle.dump(data, outfile)
+            outfile.close()
+        else:                               # else write to standard output
+            pickle.dump(data, sys.stdout.buffer)
