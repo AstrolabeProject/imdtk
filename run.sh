2ab12bb36c77404f10c1dd980e6e05af97a8eea3
Tom Hicks <hickst@tohono.com>
2020-06-03 18:23:26 -0700

    Add some more test pipelines.

--- run.sh
+++ run.sh
@@ -7,15 +7,22 @@
 
 # echo "ARGS=$*"
 
-# Bad calls:
-#
-# headers -ff /images/DC_191217/F356W.fits -ofmt csv
-# headers -ff /images/DC_191217/F356W.fits | aliases -v -g -ofmt csv
+#############
+# Bad pipes:
+#############
 
-# Good calls:
-#
+# headers -ff /images/DC_191217/F356W.fits -ofmt xxx
+# headers -ff /images/DC_191217/F356W.fits | aliases -v -g -ofmt xxx
+
+
+#############
+# Good pipes:
+#############
+
 # headers -ff /images/DC_191217/F356W.fits
 # headers -ff /images/DC_191217/F356W.fits -g
+# headers -ff /images/DC_191217/F356W.fits -of /work/F356W_headers.json
+
 # headers -ff /images/DC_191217/F356W.fits | aliases -v
-headers -ff /images/DC_191217/F356W.fits | aliases -d -g
-# headers -ff /images/DC_191217/F356W.fits | aliases -v -g
+# headers -ff /images/DC_191217/F356W.fits | aliases -d -g
+headers -ff /images/DC_191217/F356W.fits | aliases -v -g
