ef0d44390decb313bd5220556caf78bd78f8ce15
Tom Hicks <hickst@tohono.com>
2020-06-03 17:59:08 -0700

    Add/update test pipelines. Separate valid from invalid.

--- run.sh
+++ run.sh
@@ -7,7 +7,15 @@
 
 # echo "ARGS=$*"
 
+# Bad calls:
+#
+# headers -ff /images/DC_191217/F356W.fits -ofmt csv
+# headers -ff /images/DC_191217/F356W.fits | aliases -v -g -ofmt csv
+
+# Good calls:
+#
 # headers -ff /images/DC_191217/F356W.fits
-# headers -ff /images/DC_191217/F356W.fits | aliases -v -os file -ofmt csv
+# headers -ff /images/DC_191217/F356W.fits -g
 # headers -ff /images/DC_191217/F356W.fits | aliases -v
-headers -ff /images/DC_191217/F356W.fits | aliases -v -os file
+headers -ff /images/DC_191217/F356W.fits | aliases -d -g
+# headers -ff /images/DC_191217/F356W.fits | aliases -v -g
