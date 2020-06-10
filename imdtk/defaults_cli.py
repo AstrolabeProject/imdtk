bed73c40ebc72fb83bf822fd0f7ce94bc6af9a54
Tom Hicks <hickst@tohono.com>
2020-06-09 20:52:35 -0700

    Create defaults tool and CLI. Initial working version uses textual fields reader.

---
+++ imdtk/defaults_cli.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+#
+# Module to add defaults (fields) for the fields in a FITS-derived metadata structure.
+#   Written by: Tom Hicks. 6/9/20.
+#   Last Modified: Initial creation.
+#
+import os, sys
+import logging as log
+import argparse
+
+import imdtk.cli_utils as cli_utils
+from config.settings import LOG_LEVEL
+from imdtk.core.file_utils import validate_file_path
+from imdtk.tools.defaults import DEFAULT_FIELDS_FILEPATH, DefaultsTool
+from imdtk.tools.i_tool import OUTPUT_EXTENTS
+
+
+# Program name for this tool.
+TOOL_NAME = 'defaults'
+
+# Version of this tool.
+VERSION = '0.0.1'
+
+
+def main (argv=None):
+    """
+    The main method for the tool. This method is called from the command line,
+    processes the command line arguments and calls into the ImdTk library to do its work.
+    This main method takes no arguments so it can be called by setuptools.
+    """
+
+    # the main method takes no arguments so it can be called by setuptools
+    if (argv is None):                      # if called by setuptools
+        argv = sys.argv[1:]                 # then fetch the arguments from the system
+
+    # setup logging configuration
+    log.basicConfig(level=LOG_LEVEL)
+
+    # setup command line argument parsing and add shared arguments
+    parser = argparse.ArgumentParser(
+        prog=TOOL_NAME,
+        formatter_class=argparse.RawTextHelpFormatter,
+        description='Add defaults for the fields of a metadata structure and output it.'
+    )
+
+    cli_utils.add_shared_arguments(parser, TOOL_NAME, VERSION)
+    cli_utils.add_output_arguments(parser, TOOL_NAME, VERSION)
+    cli_utils.add_input_arguments(parser, TOOL_NAME, VERSION)
+
+    # add arguments specific to this module
+    parser.add_argument(
+        '-fi', '--field-info', dest='fields_file', metavar='filepath',
+        default=argparse.SUPPRESS,
+        help='Field information file for fields to be processed [default: "/imdtk/config/jwst-fields"]'
+    )
+
+    # actually parse the arguments from the command line
+    args = vars(parser.parse_args(argv))
+
+    # if debugging, set verbose and echo input arguments
+    if (args.get('debug')):
+        args['verbose'] = True              # if debug turn on verbose too
+        print("({}.main): ARGS={}".format(TOOL_NAME, args))
+
+    # filter the given input file path for validity
+    input_file = args.get('input_file')
+    if (input_file):                        # if input file given, check it
+        if (not validate_file_path(input_file, OUTPUT_EXTENTS)):
+            print("({}): A readable, valid input file must be given. Exiting...".format(TOOL_NAME),
+                  file=sys.stderr)
+            sys.exit(20)
+
+    # add additional arguments to args
+    args['TOOL_NAME'] = TOOL_NAME
+    args['VERSION'] = VERSION
+
+    # call the tool layer to process the given, validated input file
+    tool = DefaultsTool(args)
+    tool.process_and_output()
+    tool.cleanup()
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ imdtk/tools/defaults.py
@@ -0,0 +1,143 @@
+#
+# Class to add defaults (fields) for the fields in a FITS-derived metadata structure.
+#   Written by: Tom Hicks. 6/9/2020.
+#   Last Modified: Initial working version: uses textual fields reader.
+#
+import os
+import sys
+import configparser
+# import datetime     # REMOVE: unused?
+import json
+import logging as log
+
+from config.settings import CONFIG_DIR
+from imdtk.tools.i_tool import IImdTool, STDIN_NAME, STDOUT_NAME
+import imdtk.core.fields_file_reader as fields_file_reader
+
+
+# Default resource file for default field values.
+DEFAULT_FIELDS_FILEPATH = "{}/jwst-fields.txt".format(CONFIG_DIR)
+
+
+class DefaultsTool (IImdTool):
+    """ Class which adds defaults for the fields of a metadata structure. """
+
+    def __init__(self, args):
+        """
+        Constructor for class which adds defaults for the fields of a metadata structure.
+        """
+
+        # Display name of this tool
+        self.TOOL_NAME = args.get('TOOL_NAME') or 'defaults'
+
+        # Configuration parameters given to this class.
+        self.args = args
+
+        # Verbose setting: when true, show extra information about program operation.
+        self._VERBOSE = args.get('verbose', False)
+
+        # Debug setting: when true, show internal information for debugging.
+        self._DEBUG = args.get('debug', False)
+
+
+    #
+    # Concrete methods implementing ITool abstract methods
+    #
+
+    def cleanup (self):
+        """ Do any cleanup/shutdown tasks necessary for this instance. """
+        if (self._DEBUG):
+            print("({}.cleanup)".format(self.TOOL_NAME))
+
+
+    def process_and_output (self):
+        """ Perform the main work of the tool and output the results in the selected format. """
+        metadata = self.process()
+        if (metadata):
+            self.output_results(metadata)
+
+
+    def process (self):
+        """
+        Perform the main work of the tool and return the results as a Python structure.
+        """
+        if (self._DEBUG):
+            print("({}.process): ARGS={}".format(self.TOOL_NAME, self.args))
+
+        # load the field information from a given file path or a default resource path
+        fields_file = self.args.get('fields_file') or DEFAULT_FIELDS_FILEPATH
+        defaults = self.load_defaults(fields_file)
+
+        # process the given, already validated input file
+        input_file = self.args.get('input_file')
+        if (input_file is None):
+            infile = sys.stdin
+        else:
+            infile = open(infile, 'r')
+
+        if (self._VERBOSE):
+            if (infile == sys.stdin):
+                print("({}): Processing metadata from {}".format(self.TOOL_NAME, STDIN_NAME))
+            else:
+                print("({}): Processing metadata file '{}'".format(self.TOOL_NAME, infile.name))
+
+        try:
+            in_fmt = self.args.get('input_format') or 'json'
+            if (in_fmt == 'json'):
+                metadata = json.load(infile)
+            else:
+                errMsg = "({}.process): Invalid input format '{}'.".format(self.TOOL_NAME, in_fmt)
+                log.error(errMsg)
+                raise ValueError(errMsg)
+
+            metadata['defaults'] = defaults # add defaults dictionary to metadata
+            return metadata                 # return the results of processing
+
+        except Exception as ex:
+            errMsg = "({}.process): Exception while reading metadata from file '{}': {}.".format(self.TOOL_NAME, infile, ex)
+            log.error(errMsg)
+            raise RuntimeError(errMsg)
+
+
+    def output_results (self, metadata):
+        """ Output the given metadata in the selected format. """
+        genfile = self.args.get('gen_file_path')
+        outfile = self.args.get('output_file')
+        out_fmt = self.args.get('output_format') or 'json'
+
+        if (out_fmt == 'json'):
+            if (genfile):                   # if generating the output filename/path
+                fname = metadata.get('file_info').get('file_name')
+                outfile = self.gen_output_file_path(fname, out_fmt, self.TOOL_NAME)
+                self.output_JSON(metadata, outfile)
+            elif (outfile is not None):     # else if using the given filepath
+                self.output_JSON(metadata, outfile)
+            else:                           # else using standard output
+                self.output_JSON(metadata)
+
+        else:
+            errMsg = "({}.process): Invalid output format '{}'.".format(self.TOOL_NAME, out_fmt)
+            log.error(errMsg)
+            raise ValueError(errMsg)
+
+        if (self._VERBOSE):
+            out_dest = outfile if (outfile) else STDOUT_NAME
+            print("({}): Results output to '{}'".format(self.TOOL_NAME, out_dest))
+
+
+
+    #
+    # Non-interface (tool-specific) Methods
+    #
+
+    def load_defaults (self, fields_file):
+        """ Load field defaults from the given fields information filepath. """
+        if (self._VERBOSE):
+            print("({}.load_defaults): Loading from fields info file '{}'".format(self.TOOL_NAME, fields_file))
+
+        defaults = fields_file_reader.load(fields_file)
+
+        if (self._VERBOSE):
+            print("({}.load_defaults): Read {} field defaults.".format(self.TOOL_NAME, len(defaults)))
+
+        return defaults
