994436612bef8ed882932e56734796b00636581a
Tom Hicks <hickst@tohono.com>
2020-06-12 15:26:41 -0700

    Fix: add call to add collection arguments. Add collection arg test to runit.

--- imdtk/oc_calc_cli.py
+++ imdtk/oc_calc_cli.py
@@ -2,7 +2,7 @@
 #
 # Module to calculate values for the ObsCore fields in a FITS-derived metadata structure.
 #   Written by: Tom Hicks. 6/11/2020.
-#   Last Modified: Refactor/use FITS file arguments to/from CLI utils.
+#   Last Modified: Fix: add call to add collection arguments.
 #
 import os, sys
 import logging as log
@@ -19,7 +19,7 @@ from imdtk.tools.oc_calc import ObsCoreCalcTask
 TOOL_NAME = 'oc_calc'
 
 # Version of this tool.
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 
 
 def main (argv=None):
@@ -47,6 +47,7 @@ def main (argv=None):
     cli_utils.add_output_arguments(parser, TOOL_NAME, VERSION)
     cli_utils.add_input_arguments(parser, TOOL_NAME, VERSION)
     cli_utils.add_fits_file_arguments(parser, TOOL_NAME, VERSION)
+    cli_utils.add_collection_arguments(parser, TOOL_NAME, VERSION)
 
     # actually parse the arguments from the command line
     args = vars(parser.parse_args(argv))
