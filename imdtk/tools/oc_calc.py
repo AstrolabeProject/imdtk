15cee365731e4df0faae51f5a55028c65f1b0380
Tom Hicks <hickst@tohono.com>
2020-06-12 18:16:11 -0700

    Implement calculation logic.

--- imdtk/tools/oc_calc.py
+++ imdtk/tools/oc_calc.py
@@ -1,7 +1,7 @@
 #
 # Class to calculate values for the ObsCore fields in a FITS-derived metadata structure.
 #   Written by: Tom Hicks. 6/11/2020.
-#   Last Modified: Add calc_corners and calc_spatial limits.
+#   Last Modified: Implement calculation logic sequence.
 #
 import os, sys
 import json
@@ -9,12 +9,24 @@ import logging as log
 
 from astropy.io import fits
 
-import imdtk.core.fits_utils as fits_utils
+from config.settings import IMAGE_FETCH_PREFIX, IMAGES_DIR
 from imdtk.tools.i_tool import IImdTool, STDIN_NAME, STDOUT_NAME
+import imdtk.core.fits_utils as fits_utils
 
 
 class ObsCoreCalcTask (IImdTool):
     """ Class to calculate values for ObsCore fields in a metadata structure. """
+
+    # Spatial resolutions for NIRCam filters, keyed by filter name.
+    JWST_FILTER_RESOLUTIONS = {
+        'F070W': 0.030, 'F090W': 0.034,  'F115W': 0.040,  'F140M': 0.048, 'F150W': 0.050,
+        'F162M': 0.055, 'F164N': 0.056,  'F150W2': 0.046, 'F182M': 0.062, 'F187N': 0.064,
+        'F200W': 0.066, 'F210M': 0.071,  'F212N': 0.072,  'F250M': 0.084, 'F277W': 0.091,
+        'F300M': 0.100, 'F322W2': 0.097, 'F323N': 0.108,  'F335M': 0.111, 'F356W': 0.115,
+        'F360M': 0.120, 'F405N': 0.136,  'F410M': 0.137,  'F430M': 0.145, 'F444W': 0.145,
+        'F460M': 0.155, 'F466N': 0.158,  'F470N': 0.160,  'F480M': 0.162
+    }
+
 
     def __init__(self, args):
         """ Constructor for class which calculates values for ObsCore fields in a metadata structure. """
@@ -86,16 +98,11 @@ class ObsCoreCalcTask (IImdTool):
         input_format = self.args.get('input_format') or DEFAULT_INPUT_FORMAT
         metadata = self.input_JSON(input_file, input_format, self.TOOL_NAME)
 
-        # perform the various calculations and accumulate them
-        calculations = dict()
+        # try to produce values for each of the desired result fields
+        calculated = self.calculate_results(wcs_info, metadata)
+        metadata['calculated'] = calculated # add calculations to metadata
 
-        self.calc_scale(wcs_info, calculations)
-        self.calc_corners(wcs_info, calculations)
-        # TODO: IMPLEMENT more calculations LATER
-
-        metadata['calculated'] = calculations # add calculations to metadata
-
-        return metadata                 # return the results of processing
+        return metadata                     # return the results of processing
 
 
     def output_results (self, metadata):
@@ -127,6 +134,31 @@ class ObsCoreCalcTask (IImdTool):
     #
     # Non-interface and/or Tool-specific Methods
     #
+
+    def calculate_results (self, wcs_info, metadata):
+        """
+        Try to produce a value for each of the fields desired in the results.
+        Values are extracted, calculated, or defaulted from existing metadata.
+        Return a dictionary of fields and values for all fields for which
+        a value was able to be produced.
+        """
+        calculations = dict()               # structure for calculated results
+
+        # calculate some initial values from the FITS file WCS information
+        self.calc_scale(wcs_info, calculations)
+        self.calc_corners(wcs_info, calculations)
+
+        # copy any FITS header fields that were aliased to desired result fields
+        self.copy_aliased(metadata, calculations)
+
+        # calculate any values which require special casing
+        self.calc_special_case_fields(wcs_info, metadata, calculations)
+
+        # try to produce a value for each desired field
+        self.calc_field_values(wcs_info, metadata, calculations)
+
+        return calculations
+
 
     def calc_corners (self, wcs_info, calculations):
         """
@@ -164,6 +196,74 @@ class ObsCoreCalcTask (IImdTool):
             calculations['im_scale'] = scale[0]
 
 
+    def calc_field_values (self, wcs_info, metadata, calculations):
+        """
+        For all desired fields, compute (or recompute) a value for each field.
+        Values are extracted, calculated, or defaulted from existing metadata.
+        If a value is produced for a field, store the field and its value into
+        the given calculations structure.
+        """
+        desired = metadata.get('fields_info').keys()  # make list of desired fields
+        for fieldname in desired:
+            self.calc_field_value(fieldname, wcs_info, metadata, calculations)
+            if (fieldname not in calculations): # if field still has no value
+                self.set_default(fieldname, metadata, calculations)
+
+
+    def calc_field_value (self, field_name, wcs_info, metadata, calculations):
+        """
+        Provide the opportunity to calculate (or recalculate) a value for the named field.
+        """
+        if (field_name in ['s_ra', 's_dec']):
+            self.calc_wcs_coordinates(wcs_info, calculations)
+
+        elif (field_name in ['im_naxis1', 'im_naxis2']):
+            if (calculations.get('s_xel1') is not None):
+                calculations['im_naxis1'] = calculations.get('s_xel1')
+            if (calculations.get('s_xel2') is not None):
+                calculations['im_naxis2'] = calculations.get('s_xel2')
+
+        elif (field_name == 's_resolution'):
+            self.calc_spatial_resolution(calculations,
+                                         filter_resolutions=self.JWST_FILTER_RESOLUTIONS)
+
+        elif (field_name == 'im_pixtype'):
+            self.calc_pixtype(metadata, calculations)
+
+        elif (field_name == 'access_url'):
+            # create URL to fetch the file from the server
+            file_path = metadata.get('file_info').get('file_path')
+            if (file_path is not None):
+                image_path = "{0}{1}{2}".format(IMAGE_FETCH_PREFIX, IMAGES_DIR, file_path)
+                calculations['access_url'] = image_path
+
+        elif (field_name == 'instrument_name'):
+            # (re)create instrument name from NIRCam + MODULE value
+            module = calculations.get('nircam_module')
+            inst_name = "NIRCam-{}".format(module) if (module is not None) else "NIRCam"
+            calculations['instrument_name'] = inst_name
+
+        elif (field_name == 'target_name'):
+            filename = metadata.get('file_info').get('file_name')
+            if (filename is not None):
+                if (filename.lower().startswith("goods_s")):
+                    calculations['target_name'] = "goods_south"
+                elif (filename.lower().startswith("goods_n")):
+                    calculations['target_name'] = "goods_north"
+                else:
+                    calculations['target_name'] = "UNKNOWN"
+
+
+    def calc_pixtype (self, metadata, calculations):
+        """
+        Calculate the value string for the ObsCore im_pixeltype field based on the value
+        of the FITS BITPIX keyword.
+        """
+        bitpix = metadata.get('headers').get('BITPIX')
+        if (bitpix):
+            calculations['im_pixtype'] = fits_utils.lookup_pixtype(bitpix)
+
+
     def calc_spatial_limits (self, corners, calculations):
         """
         Calculate the min/max of the RA and DEC axes from the given list of four
@@ -178,6 +278,65 @@ class ObsCoreCalcTask (IImdTool):
         calculations['spat_hilimit2'] = max(decs)
 
 
+    def calc_spatial_resolution (self, calculations, filter_resolutions=None):
+        """
+        Use the filter value to determine the spatial resolution based on a NIRCam
+        filter-resolution table.
+        """
+        filt = calculations.get('filter')   # may not have filter field
+        if (filt and filter_resolutions):   # may not know filter resolutions
+            resolution = filter_resolutions.get(filt)
+            if (resolution):
+                calculations['s_resolution'] = resolution
+
+
+    def calc_special_case_fields (self, wcs_info, metadata, calculations):
+        """ Perform special case calculations. """
+
+        #  special case: correct the t_exptime zero value with a
+        #                default of 1347.0 per Eiichi Egami 20190626.
+        #
+        t_exptime = calculations.get('t_exptime')
+        if (t_exptime == 0.0):
+            calculations['t_exptime'] = 1347.0
+
+        # special case: if given a collection name argument, then use it.
+        collection = self.args.get('collection')
+        if (collection):
+            calculations['obs_collection'] = collection
+
+
+    def calc_wcs_coordinates (self, wcs_info, calculations):
+        """
+        Extract the WCS coordinates for the reference pixel of the current image file.
+        Sets both s_ra and s_dec fields simultaneously when either field is processed.
+        This method assumes that neither s_ra nor s_dec fields have a value yet and it will
+        overwrite current values for both s_ra and s_dec if that assumption is not valid.
+        """
+        if (('s_ra' in calculations) and ('s_dec' in calculations)): # avoid repetition
+            return                          # exit out now
+
+        crval = list(wcs_info.wcs.crval)
+        ctype = list(wcs_info.wcs.ctype)
+
+        if ((len(crval) > 1) and (len(ctype) > 1)):
+            if (ctype[0].startswith('RA')):      # if CRVAL1 has the RA value
+                calculations['s_ra']  = crval[0] # put CRVAL1 value into s_ra
+                calculations['s_dec'] = crval[1] # put CRVAL2 value into s_dec
+            elif (ctype[0].startswith('DEC')):   # else if CRVAL1 has the DEC value
+                calculations['s_dec'] = crval[0] # put CRVAL1 value into s_dec
+                calculations['s_ra']  = crval[1] # put CRVAL2 value into s_ra
+            else:
+                errMsg = "({}.calc_wcs_coords): Unable to assign RA/DEC axes from ctype={}".format(self.TOOL_NAME, ctype)
+                log.error(errMsg)
+                raise RuntimeError(errMsg)
+
+
+    def copy_aliased (self, metadata, calculations):
+        """ Copy all aliased fields to the calculations structure. """
+        calculations.update(metadata.get('aliased'))
+
+
     def set_corner_field (self, calculations, ra_field_key, dec_field_key, corner):
         """
         Store the given corner coordinates [RA, DEC] into fields of the calculations
@@ -186,3 +345,13 @@ class ObsCoreCalcTask (IImdTool):
         if (len(corner) > 1):               # each corner is a list of RA, DEC
             calculations[ra_field_key] = corner[0]
             calculations[dec_field_key] = corner[1]
+
+
+    def set_default (self, fieldname, metadata, calculations):
+        """
+        Lookup the default value for the given field name and, if found, store the
+        field and default value in the calculations structure.
+        """
+        default = metadata.get('defaults').get(fieldname)
+        if (default is not None):
+            calculations[fieldname] = default
