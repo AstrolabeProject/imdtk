d6d5b700b1a4b2fe320a0d2c4ee175059f548b58
Tom Hicks <hickst@tohono.com>
2020-06-13 18:29:43 -0700

    Update setup and add examples to runit script for new miss report.

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
             'aliases = imdtk.aliases_cli:main',
             'fields_info = imdtk.fields_info_cli:main',
             'headers = imdtk.headers_cli:main',
-            'jwst_oc_calc = imdtk.jwst_oc_calc_cli:main'
+            'jwst_oc_calc = imdtk.jwst_oc_calc_cli:main',
+            'miss_report = imdtk.miss_report_cli:main'
         ]
     },
 )
