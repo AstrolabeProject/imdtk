0ce507a404124d5640360a4890c6c6c81fb58313
Tom Hicks <hickst@tohono.com>
2020-06-17 18:25:55 -0700

    Enhance/organize runit/runpipes scripts with messages, sections, pickle examples.

--- tests/runpipes.sh
+++ tests/runpipes.sh
@@ -3,7 +3,7 @@
 # Shell script to run pipelines from inside the imdtk Docker file
 # using the tools it contains. This script should be mounted by
 # the container and runs within its environment.
-# 
+#
 
 # echo "ARGS=$*"
 
@@ -13,75 +13,117 @@
 
 # headers -ff /images/DC_191217/F356W.fits -ofmt xxx
 # headers -ff /images/DC_191217/F356W.fits | aliases -v -g -ofmt xxx
+# headers -ff /images/DC_191217/F356W.fits | aliases -v | pickle_sink -v
 
 
 #############
 # Good pipes:
 #############
 
-# All tools: show versions
+echo "All tools: show versions"
 headers -d --version
 aliases -d --version
 fields_info -d --version
 jwst_oc_calc -d --version
 miss_report -d --version
 no_op -d --version
+pickle_sink -d --version
 
-# Headers only, to standard output:
+echo "--------------------------------------------"
+echo "Last tool verbose, to STANDARD OUTPUT:"
+# headers -ff /images/DC_191217/F356W.fits | aliases | fields_info -v
+
+echo "--------------------------------------------"
+echo "Headers only, verbose, to STANDARD OUTPUT:"
 # headers -v -ff /images/JADES/goods_s_F356W_2018_08_30.fits
 
-# Headers only, named filename:
+echo "--------------------------------------------"
+echo "Headers only, named filename:"
 headers -v -ff /images/JADES/goods_s_F356W_2018_08_30.fits -of /work/JADES_F356W_headers.json
 
-# Headers only, generated filename:
+echo "--------------------------------------------"
+echo "Headers only, generated filename:"
 headers -v -ff /images/JADES/goods_s_F356W_2018_08_30.fits -g
 
-# Headers to aliases:
+echo "--------------------------------------------"
+echo "Headers to aliases:"
 headers -v -ff /images/DC_191217/F356W.fits | aliases -v -g
 
-# Headers to aliases to fields_info:
+echo "--------------------------------------------"
+echo "Headers to aliases to fields_info:"
 headers -v -ff /images/DC_191217/F356W.fits | aliases -v | fields_info -v -g
 
-# Headers to aliases to fields_info to jwst_oc_calc:
+echo "--------------------------------------------"
+echo "Headers to aliases to fields_info to jwst_oc_calc:"
 headers -v -ff /images/DC_191217/F356W.fits | aliases -v | fields_info -v | jwst_oc_calc -v -ff /images/DC_191217/F356W.fits -g
 
-# Headers to aliases to fields_info to jwst_oc_calc to miss_report:
+echo "--------------------------------------------"
+echo "Headers to aliases to fields_info to jwst_oc_calc to miss_report:"
 headers -v -ff /images/DC_191217/F356W.fits | aliases -v | fields_info -v | jwst_oc_calc -v -ff /images/DC_191217/F356W.fits | miss_report -v -g
 
-# Last tool debug and generate filename:
+echo "--------------------------------------------"
+echo "Headers to aliases to fields_info to jwst_oc_calc to miss_report, specify COLLECTION:"
+headers -v -ff /images/DC_191217/F356W.fits | aliases -v | fields_info -v | jwst_oc_calc -v -c TEST_COLL -ff /images/DC_191217/F356W.fits | miss_report -v -g
+
+echo "--------------------------------------------"
+echo "Headers to aliases to fields_info to jwst_oc_calc to miss_report to no_op:"
+headers -v -ff /images/DC_191217/F356W.fits | aliases -v | fields_info -v | jwst_oc_calc -v -ff /images/DC_191217/F356W.fits | miss_report -v | no_op -v -g
+
+echo "--------------------------------------------"
+echo "Headers to aliases to fields_info to jwst_oc_calc to miss_report to no_op to pickle_sink:"
+headers -v -ff /images/DC_191217/F356W.fits | aliases -v | fields_info -v | jwst_oc_calc -v -ff /images/DC_191217/F356W.fits | miss_report -v | no_op -v | pickle_sink -v -g
+
+echo "--------------------------------------------"
+echo "Headers to aliases to fields_info to jwst_oc_calc to miss_report to no_op to pickle_sink, all DEBUG:"
+headers -d -ff /images/DC_191217/F356W.fits | aliases -d | fields_info -d | jwst_oc_calc -d -ff /images/DC_191217/F356W.fits | miss_report -d | no_op -d | pickle_sink -d -g
+
+echo "--------------------------------------------"
+echo "Headers to aliases to fields_info to jwst_oc_calc to miss_report to no_op to pickle_sink, all SILENT:"
+headers -ff /images/DC_191217/F356W.fits | aliases | fields_info | jwst_oc_calc -ff /images/DC_191217/F356W.fits | miss_report | no_op | pickle_sink -g
+
+echo "--------------------------------------------"
+echo "Pickle pipeline, generated filename, verbose:"
+headers -v -ff /images/DC_191217/F356W.fits | aliases -v | fields_info -v | jwst_oc_calc -v -ff /images/DC_191217/F356W.fits | miss_report -v | pickle_sink -v -g
+
+echo "--------------------------------------------"
+echo "Pickle pipeline, named filename:"
+headers -v -ff /images/DC_191217/F356W.fits | aliases -v | fields_info -v | jwst_oc_calc -v -ff /images/DC_191217/F356W.fits | miss_report -v | pickle_sink -v -of /work/DC_F356W.pickle
+
+echo "--------------------------------------------"
+echo "Last tool debug and generate filename:"
 headers -ff /images/DC_191217/F356W.fits | aliases -d -g
 
-# Explicit intermediate file pipeline:
-# headers -d -ff /images/DC_191217/F356W.fits -of /work/h.json
-# aliases -d -if /work/h.json -of /work/ha.json
-# fields_info -d -if /work/ha.json -of /work/hafi.json
-# jwst_oc_calc -d -ff /images/DC_191217/F356W.fits -if /work/hafi1.json -of /work/hafijoc1.json
-# miss_report -d -if /work/hafijoc1.json -of /work/hafijocmr1.json
+echo "--------------------------------------------"
+echo "Explicit intermediate file pipeline:"
+headers -d -ff /images/DC_191217/F356W.fits -of /work/h.json
+aliases -d -if /work/h.json -of /work/ha.json
+fields_info -d -if /work/ha.json -of /work/hafi.json
+jwst_oc_calc -d -ff /images/DC_191217/F356W.fits -if /work/hafi.json -of /work/hafijoc.json
+miss_report -d -if /work/hafijoc.json -of /work/hafijocmr.json
+no_op -d -if /work/hafijocmr.json -of /work/hafijocmrnop.json
+pickle_sink -d -if /work/hafijocmrnop.json -of /work/hafijocmrnoppk1.pickle
 
-# Explicit intermediate file pipeline with non-json names:
+echo "--------------------------------------------"
+echo "Explicit intermediate file pipeline with non-json names:"
 headers -d -ff /images/DC_191217/F356W.fits -of /work/h1
 aliases -d -if /work/h1 -of /work/ha1
 fields_info -d -if /work/ha1 -of /work/hafi1
 jwst_oc_calc -d -ff /images/DC_191217/F356W.fits -if /work/hafi1 -of /work/hafijoc1
 miss_report -d -if /work/hafijoc1 -of /work/hafijocmr1
+no_op -d -if /work/hafijocmr1 -of /work/hafijocmrnop1
+pickle_sink -d -if /work/hafijocmrnop1 -of /work/hafijocmrnoppk1
 
-# NO-OP: do nothing, verbose:
+echo "--------------------------------------------"
+echo "NO-OPs in various positions, verbose:"
 headers -v -ff /images/DC_191217/F356W.fits | aliases -v | no_op -v -g
 headers -v -ff /images/DC_191217/F356W.fits | no_op -v | aliases -v -g
+
+echo "--------------------------------------------"
+echo "NO-OPs in various positions, debug:"
+headers -v -ff /images/DC_191217/F356W.fits | aliases -v | no_op -v -g
 headers -d -ff /images/DC_191217/F356W.fits | no_op -d | aliases -d -g
-headers -v -ff /images/DC_191217/F356W.fits | aliases -v | fields_info -v | jwst_oc_calc -v -ff /images/DC_191217/F356W.fits | miss_report -v | no_op -v -g
 
-# Last tool verbose, to standard output:
-# headers -ff /images/DC_191217/F356W.fits | aliases | fields_info -v
-
-# All tools verbose, specify collection:
-headers -v -ff /images/DC_191217/F356W.fits | aliases -v | fields_info -v | jwst_oc_calc -v -c TEST_COLL -ff /images/DC_191217/F356W.fits -g
-
-# All tools silent:
-headers -ff /images/DC_191217/F356W.fits | aliases | fields_info | jwst_oc_calc -ff /images/DC_191217/F356W.fits | miss_report -g
-
-# All tools debug:
-headers -d -ff /images/DC_191217/F356W.fits | aliases -d | fields_info -d | jwst_oc_calc -d -ff /images/DC_191217/F356W.fits | miss_report -d -g
-
-# All tools verbose:
-headers -v -ff /images/DC_191217/F356W.fits | aliases -v | fields_info -v | jwst_oc_calc -v -ff /images/DC_191217/F356W.fits | miss_report -v | no_op -v -g
+echo "--------------------------------------------"
+echo "Current development:"
+headers -d -ff /images/DC_191217/F356W.fits | aliases -d | fields_info -d | jwst_oc_calc -d -ff /images/DC_191217/F356W.fits | miss_report -d | pickle_sink -d -g
+headers -v -ff /images/DC_191217/F356W.fits | aliases -v | fields_info -v | jwst_oc_calc -v -ff /images/DC_191217/F356W.fits | miss_report -v | pickle_sink -v -g
