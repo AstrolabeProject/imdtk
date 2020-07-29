49bc357bbc5989e16931e817a89af0530f119764
Tom Hicks <hickst@tohono.com>
2020-07-28 21:14:14 -0700

    Add tests for sql_insert_str, sql_insert_hybrid_str, sql4_hybrid_table_insert and stubs for sql_create_table and sql_create_table_str.

--- tests/imdtk/core/test_pg_sql.py
+++ tests/imdtk/core/test_pg_sql.py
@@ -1,9 +1,11 @@
 # Tests for the PostgreSQL interface module.
 #   Written by: Tom Hicks. 7/25/2020.
-#   Last Modified: Update tests for list_catalog_tables rename.
+#   Last Modified: Add tests for sql_insert_str, sql_insert_hybrid_str, sql4_hybrid_table_insert
+#                  and stubs for sql_create_table and sql_create_table_str.
 #
 import pytest
 
+from config.settings import SQL_FIELDS_HYBRID
 import imdtk.core.pg_sql as pgsql
 
 
@@ -18,6 +20,31 @@ class TestPgSql(object):
         # 'db_user': 'astrolabe',
         'db_schema_name': 'sia',
         'db_uri': 'postgresql://astrolabe:changeMe@pgdb:5432/vos'
+    }
+
+    datad = {
+        "SIMPLE": True,
+        "BITPIX": -64,
+        "NAXIS": 2,
+        "NAXIS1": 9791,
+        "NAXIS2": 4305,
+        "INSTRUME": "JWST",
+        "TIMESYS": "UTC",
+        "WCSAXES": 2,
+        "CRVAL1": 53.157662568,
+        "CRVAL2": -27.8075199236
+    }
+
+    datad_hyb = {
+        "obs_collection": "JWST",
+        "s_dec": 53.157662568,
+        "s_ra": -27.8075199236,
+        "is_public": 0,
+        "metadata": {
+            "file_name": "some.fits",
+            "file_size": 4305,
+            "timesys": "UTC",
+        }
     }
 
 
@@ -90,3 +117,140 @@ class TestPgSql(object):
         print(cats)
         assert cats is not None
         assert len(cats) == 0
+
+
+
+    def test_sql_create_table_empty (self):
+        tbl = pgsql.sql_create_table(self.dbargs, dict(), 'new_tbl')
+        assert tbl is not None
+        assert tbl == "-- Creating table 'new_tbl'"
+
+
+    def test_sql_create_table (self):
+        tbl = pgsql.sql_create_table(self.dbargs, dict(), 'new_tbl')
+        assert tbl is not None
+        assert tbl == "-- Creating table 'new_tbl'"
+
+
+
+    def test_sql_create_table_str_empty (self):
+        tbl = pgsql.sql_create_table_str(dict(), 'NEWTBL')
+        assert tbl is not None
+        assert tbl == "-- SQL string for creating table 'NEWTBL'"
+
+
+    def test_sql_create_table_str (self):
+        tbl = pgsql.sql_create_table_str(self.dbargs, 'NEWTBL')
+        assert tbl is not None
+        assert tbl == "-- SQL string for creating table 'NEWTBL'"
+
+
+
+    def test_sql_insert_str_empty (self):
+        sql = pgsql.sql_insert_str(dict(), 'test_table')
+        print(sql)
+        assert sql is not None
+        assert len(sql) > 0
+        assert 'insert' in sql
+        assert 'test_table' in sql
+        assert 'values' in sql
+        assert '();' in sql
+
+
+    def test_sql_insert_str (self):
+        sql = pgsql.sql_insert_str(self.datad, 'test_table')
+        print(sql)
+        assert sql is not None
+        assert len(sql) > 175               # specific to datad table
+        assert 'insert' in sql
+        assert 'test_table' in sql
+        assert 'values' in sql
+        assert 'True' in sql
+        assert 'SIMPLE' in sql
+        assert 'BITPIX' in sql
+        assert '-64' in sql
+        assert '9791' in sql
+        assert "'JWST'" in sql
+        assert '53.157662568'
+
+
+
+    def test_sql_insert_hybrid_str_empty (self):
+        sql = pgsql.sql_insert_hybrid_str(dict(), 'TESTTBL')
+        print(sql)
+        assert sql is None
+
+
+    def test_sql_insert_hybrid_str_nomd (self):
+        datad_hyb_nomd = {
+            "ob_collection": "JWST",
+            "s_dec": 53.157662568,
+            "s_ra": -27.8075199236,
+            "is_public": 0
+        }
+        sql = pgsql.sql_insert_hybrid_str(datad_hyb_nomd, 'TESTTBL')
+        print(sql)
+        assert sql is None
+
+
+    def test_sql_insert_hybrid_str (self):
+        sql = pgsql.sql_insert_hybrid_str(self.datad_hyb, 'TESTTBL')
+        print(sql)
+        assert sql is not None
+        print("LEN(sql)={}".format(len(sql)))
+        assert len(sql) > 175               # specific to datad_hyb table
+        assert 'insert' in sql
+        assert 'TESTTBL' in sql
+        assert 'values' in sql
+
+        for fld in SQL_FIELDS_HYBRID:
+            assert fld in sql
+        assert '53.157662568' in sql
+        assert '-27.8075199236' in sql
+
+        assert 'True' not in sql
+        assert 'SIMPLE' not in sql
+        assert 'BITPIX' not in sql
+
+
+
+    def test_sql4_hybrid_table_insert_empty (self):
+        sql = pgsql.sql4_hybrid_table_insert(dict(), 'TESTTBL')
+        print(sql)
+        assert sql is None
+
+
+    def test_sql4_hybrid_table_insert_nomd (self):
+        datad_hyb_nomd = {
+            "ob_collection": "JWST",
+            "s_dec": 53.157662568,
+            "s_ra": -27.8075199236,
+            "is_public": 0
+        }
+        sql = pgsql.sql4_hybrid_table_insert(datad_hyb_nomd, 'TESTTBL')
+        print(sql)
+        assert sql is None
+
+
+    def test_sql4_hybrid_table_insert (self):
+        sql = pgsql.sql4_hybrid_table_insert(self.datad_hyb, 'TESTTBL')
+        print(sql)
+        assert sql is not None
+        assert len(sql) == 2                # tuple of template, values
+
+        templ = sql[0]
+        assert 'insert' in templ
+        assert 'TESTTBL' in templ
+        assert 'values' in templ
+
+        for fld in SQL_FIELDS_HYBRID:
+            assert fld in templ
+
+        vals = sql[1]
+        assert 53.157662568 in vals
+        assert -27.8075199236 in vals
+        assert 'JWST' in vals
+
+        assert 'True' not in vals
+        assert 'SIMPLE' not in vals
+        assert 'BITPIX' not in vals
