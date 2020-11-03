a7a2613c334195518c8187e693eed7e30868bc52
Tom Hicks <hickst@tohono.com>
2020-11-02 21:08:29 -0700

    Add sanity checks, error handling, and doc strings.

--- imdtk/tasks/irods_fits_image_md.py
+++ imdtk/tasks/irods_fits_image_md.py
@@ -1,12 +1,14 @@
 #
 # Class for extracting header information from iRods-resident FITS files.
 #   Written by: Tom Hicks. 10/15/20.
-#   Last Modified: Refactor for new FITS iRods helper class.
+#   Last Modified: Add sanity checks, error handling, and doc strings.
 #
 import os
 import sys
 
 # from astropy.io import fits
+
+from irods.exception import DataObjectDoesNotExist
 
 import imdtk.exceptions as errors
 import imdtk.core.fits_irods_helper as firh
@@ -14,6 +16,7 @@ import imdtk.core.fits_utils as fits_utils
 
 from imdtk.core.fits_utils import FITS_IGNORE_KEYS
 from imdtk.tasks.i_task import IImdTask
+from imdtk.tools.cli_utils import FITS_FILE_EXIT_CODE
 
 
 class IRodsFitsImageMetadataTask (IImdTask):
@@ -29,7 +32,8 @@ class IRodsFitsImageMetadataTask (IImdTask):
 
     def cleanup (self):
         """ Do any cleanup/shutdown tasks necessary for the task instance. """
-        self.irods.cleanup()
+        if (self.irods):
+            self.irods.cleanup()
         super().cleanup
 
 
@@ -44,30 +48,50 @@ class IRodsFitsImageMetadataTask (IImdTask):
         if (self._DEBUG):
             print("({}.process): ARGS={}".format(self.TOOL_NAME, self.args), file=sys.stderr)
 
-        # TODO: add check of given iRods FITS file path LATER?
-        irff_path = self.args.get('irods_fits_file')
-        # check_irods_fits_file(irff_path, TOOL_NAME) # throw error if not found
-
-        # process the validated FITS file
         ignore_list = self.args.get('ignore_list') or fits_utils.FITS_IGNORE_KEYS
         which_hdu = self.args.get('which_hdu', 0)
 
+        # get the iRods file path of the file to be opened
+        irff_path = self.args.get('irods_fits_file')
+
+        # the specified FITS file must have a valid FITS extension
+        if (not fits_utils.is_fits_filename(irff_path)):
+            errMsg = "A readable, valid FITS image file must be specified.".format(irff_path)
+            raise errors.ProcessingError(errMsg)
+
         try:
+            # get an instance of the iRods accessor class
+            self.irods = firh.FitsIRodsHelper(self.args)
+
+            # get the specified FITS file
+            irff = self.irods.getf(irff_path, absolute=True)
+
+            # sanity check on given FITS file
+            if (irff.size < firh.FITS_BLOCK_SIZE):
+                errMsg = "File is too small to be a valid FITS file: '{}'".format(irff_path)
+                raise errors.UnsupportedTypeError(errMsg)
+
             # TODO: skip catalog files LATER:
             # with fits.open(irff_path) as hdus_list:
             #     if (fits_utils.is_catalog_file(hdus_list)):
             #         errMsg = "Skipping FITS catalog '{}'".format(irff_path)
             #         raise errors.UnsupportedTypeError(errMsg)
 
-
-            self.irods = firh.FitsIRodsHelper(self.args)
-
-            irff = self.irods.getf(irff_path, absolute=True)
-
+            # get and save some metadata ABOUT the iRods FITS file
             file_info = self.irods.get_irods_file_info(irff)
 
+            # now try to read the FITS header from the FITS file
             header = self.irods.read_header(irff)
-            hdrs = fits_utils.get_fields_from_header(header)
+            if (not header):
+                errMsg = "Unable to read image metadata headers from iRods FITS file '{}'.".format(irff_path)
+                raise errors.ProcessingError(errMsg)
+
+            else:
+                hdrs = fits_utils.get_fields_from_header(header)
+
+        except DataObjectDoesNotExist as dodne:
+            errMsg = "Unable to find the specified iRods FITS file '{}'.".format(irff_path)
+            raise errors.ProcessingError(errMsg)
 
         except OSError as oserr:
             errMsg = "Unable to read image metadata from iRods FITS file '{}': {}.".format(irff_path, oserr)
