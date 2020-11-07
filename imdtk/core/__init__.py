5876ba58d0d755f05ea13716a071ee25c131ad85
Tom Hicks <hickst@tohono.com>
2020-11-06 17:30:45 -0700

    Add FitsHeaderInfo class.

--- imdtk/core/__init__.py
+++ imdtk/core/__init__.py
@@ -2,3 +2,6 @@ import collections
 
 # class to hold an individual metadatum, a list of which is the metadata
 Metadatum = collections.namedtuple('Metadatum', ['keyword', 'value'])
+
+# class to hold an offsets and length information for FITS headers
+FitsHeaderInfo = collections.namedtuple('FitsHeaderInfo', ['offset', 'length', 'hdr'])
