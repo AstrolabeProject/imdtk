8f73d27deb526eebe813c2056d41c43bf65ef3a2
Tom Hicks <hickst@tohono.com>
2020-11-08 15:41:40 -0700

    Reduce tests by aiming higher.

--- tests/imdtk/core/test_fits_irods_helper.py
+++ tests/imdtk/core/test_fits_irods_helper.py
@@ -1,6 +1,6 @@
 # Tests for the iRods interface module.
 #   Written by: Tom Hicks. 11/5/20.
-#   Last Modified: Initial creation.
+#   Last Modified: Reduce tests by aiming higher.
 #
 import os
 import pytest
@@ -17,12 +17,17 @@ from tests import TEST_DIR
 
 class TestIRods(object):
 
-    m13_tstfyl = "{}/resources/m13.fits".format(TEST_DIR)
-    irff_hh = '/iplant/home/hickst/vos/images/HorseHead.fits'
-    hdr0_size_hh = 14400                    # size of primary header
-    hdr1_size_hh = 2880                     # size of first extension
     irff_m13 = '/iplant/home/hickst/vos/images/m13.fits'
     hdr0_size_m13 = 2880                    # size of primary header
+    hdr0_datasize_m13 = 181440              # size of primary data table
+
+    irff_hh = '/iplant/home/hickst/vos/images/HorseHead.fits'
+    hdr0_size_hh = 14400                    # size of primary header
+    hdr0_datasize_hh = 1592640              # size of primary data table
+    hdr1_size_hh = 2880                     # size of first extension
+    hdr1_datasize_hh = 40320                # size of first extension data table
+
+    irff_smallcat = '/iplant/home/hickst/vos/catalogs/small_table.fits'
 
 
     def test_create_helper_noconn (self):
@@ -63,102 +68,72 @@ class TestIRods(object):
         assert env_file == DEFAULT_IRODS_ENV_FILEPATH
 
 
-
-    def test_calc_data_length (self):
+    def test_get_header_primary (self):
+        """
+        Also tests get_header_at, calculate_data_length, calc_data_length, read_header.
+        """
         args = { 'debug': True, 'verbose': True, 'TOOL_NAME': 'TestFitsIrodsHelper' }
         ihelper = firh.FitsIRodsHelper(args)
         print(ihelper)
+        assert ihelper is not None
+
+        # read primary header
+        irff = ihelper.getf(self.irff_m13, absolute=True)
+        header = ihelper.get_header(irff)
+        print(header)
+        assert header is not None
+        assert len(header) > 3              # 3 keywords are mandatory
+        assert header.get('SIMPLE') is True
+        assert header.get('NAXIS') == 2
+
+
+    def test_get_header_extension (self):
+        """
+        Also tests get_header_at, calculate_data_length, calc_data_length, read_header.
+        """
+        args = { 'debug': True, 'verbose': True, 'TOOL_NAME': 'TestFitsIrodsHelper' }
+        ihelper = firh.FitsIRodsHelper(args)
+        assert ihelper is not None
+
+        # read primary header
+        irff = ihelper.getf(self.irff_hh, absolute=True)
+        header = ihelper.get_header(irff, which_hdu=1)
+        print(header)
+        assert header is not None
+        assert len(header) > 5              # 5 keywords are mandatory
+        assert header.get('SIMPLE') is None
+        assert header.get('XTENSION') == 'TABLE'
+        assert header.get('GCOUNT') == 1
+
+
+    def test_is_catalog_file_m13 (self):
+        args = { 'debug': True, 'verbose': True, 'TOOL_NAME': 'TestFitsIrodsHelper' }
+        ihelper = firh.FitsIRodsHelper(args)
         assert ihelper is not None
 
         irff = ihelper.getf(self.irff_m13, absolute=True)
-        assert irff is not None
-
-        with irff.open('r+') as irff_fd:
-            hdr_info = ihelper.read_header(irff_fd, irff.size)
-            print(hdr_info)
-            assert hdr_info is not None
-            dlen = ihelper.calc_data_length(hdr_info)
-            print("DLEN={}".format(dlen))
-        assert dlen != 0
-        assert dlen == 181440
+        assert ihelper.is_catalog_file(irff) is False
+        assert ihelper.is_catalog_file(irff, which_hdu=1) is False
+        assert ihelper.is_catalog_file(irff, which_hdu=0) is False
 
 
-
-
-    # def test_read_header_at_offset0 (self):
-    #     args = { 'debug': True, 'verbose': True, 'TOOL_NAME': 'TestFitsIrodsHelper' }
-    #     ihelper = firh.FitsIRodsHelper(args)
-    #     print(ihelper)
-    #     assert ihelper is not None
-
-    #     irff = ihelper.getf(self.irff_hh, absolute=True)
-    #     assert irff is not None
-
-    #     with irff.open('r+') as irff_fd:
-    #         hdr_info = ihelper.read_header_at_offset(irff_fd, 0, irff.size)
-    #     print(hdr_info)
-    #     assert hdr_info is not None
-    #     assert hdr_info.offset == 0
-    #     assert hdr_info.length == firh.FITS_BLOCK_SIZE * 5
-    #     assert hdr_info.hdr is not None
-    #     assert isinstance(hdr_info.hdr, astropy.io.fits.header.Header)
-
-
-    # def test_read_header_at_offset1 (self):
-    #     args = { 'debug': True, 'verbose': True, 'TOOL_NAME': 'TestFitsIrodsHelper' }
-    #     ihelper = firh.FitsIRodsHelper(args)
-    #     print(ihelper)
-    #     assert ihelper is not None
-
-    #     irff = ihelper.getf(self.irff_hh, absolute=True)
-    #     assert irff is not None
-
-    #     with irff.open('r+') as irff_fd:
-    #         hdr_info = ihelper.read_header_at_offset(irff_fd, 1607040, irff.size)
-    #     print(hdr_info)
-    #     assert hdr_info is not None
-    #     assert hdr_info.offset == 1607040
-    #     assert hdr_info.length == firh.FITS_BLOCK_SIZE
-    #     assert hdr_info.hdr is not None
-    #     assert isinstance(hdr_info.hdr, astropy.io.fits.header.Header)
-
-
-    def test_read_header0 (self):
+    def test_is_catalog_file_hh (self):
         args = { 'debug': True, 'verbose': True, 'TOOL_NAME': 'TestFitsIrodsHelper' }
         ihelper = firh.FitsIRodsHelper(args)
-        print(ihelper)
         assert ihelper is not None
 
         irff = ihelper.getf(self.irff_hh, absolute=True)
-        assert irff is not None
-
-        with irff.open('r+') as irff_fd:
-            hdr_info = ihelper.read_header(irff_fd, irff.size)
-        print(hdr_info)
-        assert hdr_info is not None
-        assert hdr_info.offset == 0
-        assert hdr_info.length == self.hdr0_size_hh
-        assert hdr_info.hdr is not None
-        assert isinstance(hdr_info.hdr, astropy.io.fits.header.Header)
+        assert ihelper.is_catalog_file(irff) is True
+        assert ihelper.is_catalog_file(irff, which_hdu=1) is True
+        assert ihelper.is_catalog_file(irff, which_hdu=0) is False
 
 
-    def test_read_header1 (self):
+    def test_is_catalog_file_smallcat (self):
         args = { 'debug': True, 'verbose': True, 'TOOL_NAME': 'TestFitsIrodsHelper' }
         ihelper = firh.FitsIRodsHelper(args)
-        print(ihelper)
         assert ihelper is not None
 
-        irff = ihelper.getf(self.irff_hh, absolute=True)
-        assert irff is not None
-
-        hdr1_pos = 1607040                  # absolute offset of extension header
-
-        with irff.open('r+') as irff_fd:
-            irff_fd.seek(hdr1_pos, 0)       # seek absolute to the extension header
-            hdr_info = ihelper.read_header(irff_fd, irff.size)
-        print(hdr_info)
-        assert hdr_info is not None
-        assert hdr_info.offset == hdr1_pos
-        assert hdr_info.length == self.hdr1_size_hh
-        assert hdr_info.hdr is not None
-        assert isinstance(hdr_info.hdr, astropy.io.fits.header.Header)
+        irff = ihelper.getf(self.irff_smallcat, absolute=True)
+        assert ihelper.is_catalog_file(irff) is True
+        assert ihelper.is_catalog_file(irff, which_hdu=1) is True
+        assert ihelper.is_catalog_file(irff, which_hdu=0) is False
