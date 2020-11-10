0f05d969b3ad9d0fcaab302077afebc436130822
Tom Hicks <hickst@tohono.com>
2020-11-10 16:37:40 -0700

    Add test for to_dirpath.

--- tests/imdtk/core/test_irods_helper.py
+++ tests/imdtk/core/test_irods_helper.py
@@ -1,18 +1,16 @@
 # Tests for the iRods interface module.
 #   Written by: Tom Hicks. 10/20/20.
-#   Last Modified: Import auth and env file paths.
+#   Last Modified: Add test for to_dirpath.
 #
 import os
 import pytest
 
 import imdtk.exceptions as errors
 import imdtk.core.irods_helper as irh
+
 from config.settings import DEFAULT_IRODS_AUTH_FILEPATH, DEFAULT_IRODS_ENV_FILEPATH
 
-# from tests import TEST_DBCONFIG_FILEPATH
-
-
-class TestIRods(object):
+class TestIRodsHelper(object):
 
     def test_create_helper_noconn (self):
         args = {}
@@ -51,6 +49,16 @@ class TestIRods(object):
         assert env_file is not None
         assert env_file == DEFAULT_IRODS_ENV_FILEPATH
 
-    # def test_insert_hybrid_row_str_noval (self):
-    #     with pytest.raises(errors.ProcessingError, match='Unable to find .* required fields'):
-    #         pgsql.insert_hybrid_row_str(self.dbconfig, datad_hyb_min, 'test_tbl')
+
+    def test_to_dirpath (self):
+        args = {}
+        ihelper = irh.IRodsHelper(args, connect=False)
+
+        ihelper.to_dirpath('/') == '/'
+        ihelper.to_dirpath('.') == './'
+        ihelper.to_dirpath('x') == 'x/'
+        ihelper.to_dirpath('x/') == 'x/'
+        ihelper.to_dirpath('abc') == 'abc/'
+        ihelper.to_dirpath('abc/') == 'abc/'
+        ihelper.to_dirpath('/a/b/c') == '/a/b/c/'
+        ihelper.to_dirpath('/a/b/c/') == '/a/b/c/'
