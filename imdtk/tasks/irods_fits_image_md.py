52555f01b5148affbda685333750f89e7fd5fd8e
Tom Hicks <hickst@tohono.com>
2020-11-16 17:38:29 -0700

    Refactor to skip catalog HDUs. Use FITS block size fromfits_utils.

--- imdtk/tasks/irods_fits_image_md.py
+++ imdtk/tasks/irods_fits_image_md.py
@@ -1,7 +1,7 @@
 #
 # Class for extracting header information from iRods-resident FITS files.
 #   Written by: Tom Hicks. 10/15/20.
-#   Last Modified: Update for improved FITS iRods helper.
+#   Last Modified: Refactor to skip catalog HDUs. Use FITS block size fromfits_utils.
 #
 import os
 import sys
@@ -14,7 +14,7 @@ import imdtk.exceptions as errors
 import imdtk.core.fits_irods_helper as firh
 import imdtk.core.fits_utils as fits_utils
 
-from imdtk.core.fits_utils import FITS_IGNORE_KEYS
+from imdtk.core.fits_utils import FITS_BLOCK_SIZE, FITS_IGNORE_KEYS
 from imdtk.tasks.i_task import IImdTask
 from imdtk.tools.cli_utils import FITS_FILE_EXIT_CODE
 
@@ -48,40 +48,46 @@ class IRodsFitsImageMetadataTask (IImdTask):
         if (self._DEBUG):
             print("({}.process): ARGS={}".format(self.TOOL_NAME, self.args), file=sys.stderr)
 
-        ignore_list = self.args.get('ignore_list') or fits_utils.FITS_IGNORE_KEYS
+        # get the selection and filtering arguments
         which_hdu = self.args.get('which_hdu', 0)
+        ignore_list = self.args.get('ignore_list') or fits_utils.FITS_IGNORE_KEYS
 
-        # get the iRods file path of the file to be opened
+        # get the iRods file path argument of the file to be opened
         irff_path = self.args.get('irods_fits_file')
 
         # the specified FITS file must have a valid FITS extension
         if (not fits_utils.is_fits_filename(irff_path)):
-            errMsg = "A readable, valid FITS image file must be specified.".format(irff_path)
+            errMsg = "A readable, valid FITS image filepath must be specified.".format(irff_path)
             raise errors.ProcessingError(errMsg)
 
         try:
             # get an instance of the iRods accessor class
             self.irods = firh.FitsIRodsHelper(self.args)
 
-            # get the specified FITS file
+            # get the FITS file at the specified path
             irff = self.irods.getf(irff_path, absolute=True)
 
-            # sanity check on given FITS file
+            # sanity check on the given FITS file
             if (irff.size < firh.FITS_BLOCK_SIZE):
                 errMsg = "File is too small to be a valid FITS file: '{}'".format(irff_path)
                 raise errors.UnsupportedTypeError(errMsg)
 
-            # TODO: skip catalog files LATER:
-            # with fits.open(irff_path) as hdus_list:
-            #     if (fits_utils.is_catalog_file(hdus_list)):
-            #         errMsg = "Skipping FITS catalog '{}'".format(irff_path)
-            #         raise errors.UnsupportedTypeError(errMsg)
+            # actually read the file to get the specified header
+            header = self.irods.get_header(irff, which_hdu)
+            if (header):
+                if (self.irods.is_catalog_header(header)):
+                    errMsg = "HDU {} is a table. Skipping FITS file '{}'.".format(which_hdu, irff_path)
+                    raise errors.ProcessingError(errMsg)
 
-            # get and save some metadata ABOUT the iRods FITS file
-            file_info = self.irods.get_irods_file_info(irff)
+                # get and save some metadata ABOUT the iRods FITS file
+                file_info = self.irods.get_irods_file_info(irff)
 
-            # now try to read the FITS header from the FITS file
-            hdrs = self.irods.get_header_fields(irff, which_hdu, ignore_list)
+                # now try to read the FITS header from the FITS file
+                hdrs = fits_utils.get_fields_from_header(header, ignore_list)
+
+            else:                           # unable to read the specified header
+                errMsg = "Unable to read image metadata from HDU {} of FITS file '{}'.".format(which_hdu, irff_path)
+                raise errors.ProcessingError(errMsg)
 
         except DataObjectDoesNotExist as dodne:
             errMsg = "Unable to find the specified iRods FITS file '{}'.".format(irff_path)
