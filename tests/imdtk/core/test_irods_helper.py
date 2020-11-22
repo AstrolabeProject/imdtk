ebb777fa145df4a4554819fbe01e2b413fb9eb7a
Tom Hicks <hickst@tohono.com>
2020-11-22 16:27:53 -0700

    Add 2 tests for new getc method.

--- tests/imdtk/core/test_irods_helper.py
+++ tests/imdtk/core/test_irods_helper.py
@@ -1,16 +1,19 @@
 # Tests for the iRods interface module.
 #   Written by: Tom Hicks. 10/20/20.
-#   Last Modified: Add test for to_dirpath.
+#   Last Modified: Add 2 tests for new getc method.
 #
 import os
 import pytest
 
 import imdtk.exceptions as errors
 import imdtk.core.irods_helper as irh
+from irods.exception import CollectionDoesNotExist
 
 from config.settings import DEFAULT_IRODS_AUTH_FILEPATH, DEFAULT_IRODS_ENV_FILEPATH
 
 class TestIRodsHelper(object):
+
+    defargs = { 'debug': True, 'verbose': True, 'TOOL_NAME': 'TestIrodsHelper' }
 
     def test_create_helper_noconn (self):
         args = {}
@@ -62,3 +65,25 @@ class TestIRodsHelper(object):
         ihelper.to_dirpath('abc/') == 'abc/'
         ihelper.to_dirpath('/a/b/c') == '/a/b/c/'
         ihelper.to_dirpath('/a/b/c/') == '/a/b/c/'
+
+
+    def test_getc_badpath (self):
+        bad_path = '/iplant/home/hickst/vos/images2'
+
+        ihelper = irh.IRodsHelper(self.defargs)
+        assert ihelper is not None
+
+        with pytest.raises(CollectionDoesNotExist):
+            irdir = ihelper.getc(bad_path, absolute=True)
+
+
+    def test_getc (self):
+        img_path = '/iplant/home/hickst/vos/images'
+
+        ihelper = irh.IRodsHelper(self.defargs)
+        assert ihelper is not None
+
+        irdir = ihelper.getc(img_path, absolute=True)
+        print("IRDIR={}".format(irdir))
+        assert irdir is not None
+        assert ihelper.is_collection(irdir)
