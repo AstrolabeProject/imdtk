dcc91ea8af44caf90c42829b4b287b34d221d064
Tom Hicks <hickst@tohono.com>
2020-12-20 19:58:35 -0700

    Increment toolkit version to 0.21.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ImdTk',
-    version='0.20.1',
+    version='0.21.0',
     packages=find_packages(),
     package_data={'imdtk': ['resources/*.txt', 'resources/*.properties']},
     include_package_data=True,
